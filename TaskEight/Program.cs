acf7fc83a0b6ef36b99665d4e3ea668175e24b31
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-06 02:37:02 +0300

    Ex_004

---
+++ TaskEight/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int score = 2;
+if(number > 1){
+    while(score<=number){
+        if(score%2==0){
+            Console.WriteLine(score);
+            
+        }
+        score++;
+    }
+}
+else{
+    score=0;
+    while(score>= number){
+        if(score%2 == 0){
+            Console.WriteLine(score);
+        }
+        score--;
+    }
+}
